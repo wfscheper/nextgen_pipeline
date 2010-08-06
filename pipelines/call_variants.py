4f28a49b60869b7bd250cafdd531d2ea069aa65f
Walter Scheper <scheper@unc.edu>
2010-08-06 12:17:18 -0400

    Updated call_variants pipeline

--- pipelines/call_variants.py
+++ pipelines/call_variants.py
@@ -13,78 +13,18 @@ from ruffus import files, follows, inputs, mkdir, regex, transform
 from utils import CMD_DICT, call, pmsg, unpaired_re, unpaired_strings
 
 
-# Find candidate intervals for realignment
-def create_intervals_generator():
+def indel_genoytping_generator():
     cwd = os.getcwd()
-    for file in glob('%s/recalibrated/*.bam' % cwd):
-        filename = '%s/indel_intervals/%s' % (cwd, unpaired_strings['intervals'] % \
-                unpaired_re.search(file).groupdict())
-        yield [file, filename]
-
-@follows(mkdir('indel_intervals'))
-@files(create_intervals_generator)
-def create_intervals(input_file, output_file):
-    '''Determine indel candidate intervals'''
-    cmd_dict = CMD_DICT.copy()
-    cmd_dict['infile'] = input_file
-    cmd_dict['outfile'] = output_file
-    pmsg('Interval Creation', input_file, output_file)
-    gatk_cmd = '%(gatk)s ' + \
-            '-T RealignerTargetCreator ' + \
-            '-R %(genome)s ' + \
-            '-D %(dbsnp)s ' + \
-            '-I %(infile)s ' + \
-            '-o %(outfile)s'
-    call(gatk_cmd, cmd_dict)
-
-# Realign around possible indels
-@follows(create_intervals, mkdir('realigned'))
-@transform(create_intervals, regex(r'^(.*)/indel_intervals/(.+)\.intervals$'),
-        inputs([r'\1/recalibrated/\2.recalibrated.bam', r'\1/indel_intervals/\2.intervals']),
-        r'\1/realigned/\2.realigned.bam')
-def local_realignment(input_files, output_file):
-    '''Realign reads around candidate indels'''
-    cmd_dict = CMD_DICT.copy()
-    cmd_dict['recalibrated'] = input_files[0]
-    cmd_dict['indel_intervals'] = input_files[1]
-    cmd_dict['outfile'] = output_file
-    pmsg('Local Realignment', ', '.join(input_files), output_file)
-    gatk_cmd = '%(gatk)s ' + \
-            '-T IndelRealigner ' + \
-            '-R %(genome)s ' + \
-            '-D %(dbsnp)s ' + \
-            '-I %(recalibrated)s ' + \
-            '-targetIntervals %(indel_intervals)s ' + \
-            '--output %(outfile)s'
-    call(gatk_cmd, cmd_dict)
-    samtools_cmd = '%(samtools)s index %(outfile)s'
-    call(samtools_cmd, cmd_dict)
-
-# Fix mate info post realignment
-@follows(local_realignment, mkdir('realigned_fixmate'))
-@transform(local_realignment, regex(r'^(.*)/realigned/(.+)\.realigned\.bam$'),
-        r'\1/realigned_fixmate/\2.realigned_fixmate.bam')
-def fix_mate_realigned(input_file, output_file):
-    '''Fix mate info post-realignment'''
-    cmd_dict = CMD_DICT.copy()
-    cmd_dict['infile'] = input_file
-    cmd_dict['outfile'] = output_file
-    pmsg('Fix Mate Info', cmd_dict['infile'], cmd_dict['outfile'])
-    picard_cmd = '%(picard)s FixMateInformation ' + \
-            'I=%(infile)s ' + \
-            'O=%(outfile)s ' + \
-            'SO=coordinate ' + \
-            'MAX_RECORDS_IN_RAM=7500000 ' + \
-            'VALIDATION_STRINGENCY=SILENT'
-    call(picard_cmd, cmd_dict)
-    samtools_cmd = '%(samtools)s index %(outfile)s'
-    call(samtools_cmd, cmd_dict)
+    for infile in glob('%s/realigned_fixmate/*.bam' % cwd):
+        details_file = '%(line)s_s_%(lane)s_indels.detailed.bed' % \
+                unpaired_re.search(infile).groupdict()
+        raw_file = '%(line)s_s_%(lane)s_indels.raw.bed' % \
+                unpaired_re.search(infile).groupdict()
+        yield [infile, '%s/indels/%s' % (cwd, details_file), '%s/indels/%s' % (cwd, raw_file)]
 
 # Call Indels
-@follows(fix_mate_realigned, mkdir('indels'))
-@transform(fix_mate_realigned, regex(r'^(.*)/realigned_fixmate/(.*)\.realigned_fixmate\.bam$'),
-        r'\1/indels/\2_indels.detailed.bed',
-        r'\1/indels/\2_indels.raw.bed')
+@follows(mkdir('indels'))
+@files(indel_genoytping_generator)
 def indel_genotyping(input_file, details_file, raw_file):
     '''Call Indels'''
     cmd_dict = CMD_DICT.copy()
@@ -101,28 +41,16 @@ def indel_genotyping(input_file, details_file, raw_file):
             '--verbose'
     call(gatk_cmd, cmd_dict)
 
-# Filter Indels
-@follows(indel_genotyping, mkdir('filtered_calls'))
-@transform(indel_genotyping, regex(r'^(.*)/indels/(.*)\.detailed\.bed$'),
-        r'\1/filtered_calls/\2.filtered.bed')
-def filter_indels(input_file, output_file):
-    '''Filter indel calls'''
-    cmd_dict = CMD_DICT.copy()
-    cmd_dict['infile'] = input_file
-    cmd_dict['outfile'] = output_file
-    pmsg('Filter Indels', input_file, output_file)
-    filter_cmd = '%(filterIndels)s ' + \
-            '--calls %(infile)s ' + \
-            '--max_cons_av_mm 3.0 ' + \
-            '--max_cons_nqs_av_mm 0.5 ' + \
-            '--mode ANNOTATE ' + \
-            '> %(outfile)s'
-    call(filter_cmd, cmd_dict)
-
 # Call SNPs
-@follows(fix_mate_realigned, mkdir('snps'))
-@transform(fix_mate_realigned, regex(r'^(.*)/realigned_fixmate/(.*)\.realigned_fixmate\.bam$'),
-        r'\1/snps/\2_snps.raw.vcf')
+def snp_genoytping_generator():
+    cwd = os.getcwd()
+    for infile in glob('%s/realigned_fixmate/*.bam' % cwd):
+        outfile = '%(line)s_s_%(lane)s_snps.raw.vcf' % \
+                unpaired_re.search(infile).groupdict()
+        yield [infile, '%s/snps/%s' % (cwd, outfile)]
+
+@follows(mkdir('snps'))
+@files(snp_genoytping_generator)
 def snp_genotyping(input_file, output_file):
     '''Call SNP variants'''
     cmd_dict = CMD_DICT.copy()
@@ -139,11 +67,28 @@ def snp_genotyping(input_file, output_file):
             '-stand_call_conf 30.0'
     call(gatk_cmd, cmd_dict)
 
+# Filter Indels
+@transform(indel_genotyping, regex(r'^(.*)/indels/(.+)\.detailed\.bed$'),
+        inputs([r'\1/indels/\2.detailed.bed']),
+        r'\1/indels/\2.filtered.bed')
+def filter_indels(input_file, output_file):
+    '''Filter indel calls'''
+    cmd_dict = CMD_DICT.copy()
+    cmd_dict['infile'] = input_file
+    cmd_dict['outfile'] = output_file
+    pmsg('Filter Indels', input_file, output_file)
+    filter_cmd = '%(filterIndels)s ' + \
+            '--calls %(infile)s ' + \
+            '--max_cons_av_mm 3.0 ' + \
+            '--max_cons_nqs_av_mm 0.5 ' + \
+            '--mode ANNOTATE ' + \
+            '> %(outfile)s'
+    call(filter_cmd, cmd_dict)
+
 # Create InDel mask
-@follows(indel_genotyping)
+@follows(filter_indels)
 @transform(indel_genotyping, regex(r'^(.*)/indels/(.*)\.detailed\.bed$'),
-        inputs([r'\1/indels/\2.raw.bed']),
-        r'\1/indels/\2.mask.bed')
+        inputs([r'\1/indels/\2.raw.bed']), r'\1/indels/\2.mask.bed')
 def create_indel_mask(input_file, output_file):
     '''Create Indel Mask'''
     cmd_dict = CMD_DICT.copy()
@@ -157,11 +102,11 @@ def create_indel_mask(input_file, output_file):
     call(python_cmd, cmd_dict)
 
 # Filter SNPs
-@follows(snp_genotyping, create_indel_mask, mkdir('filtered_calls'))
+@follows(snp_genotyping, create_indel_mask)
 @transform(snp_genotyping, regex(r'^(.*)/snps/(.*)_snps.raw.vcf$'),
         inputs([r'\1/snps/\2_snps.raw.vcf',
                 r'\1/indels/\2_indels.mask.bed']),
-        r'\1/filtered_calls/\2_snps.filtered.vcf')
+        r'\1/snps/\2_snps.filtered.vcf')
 def filter_snps(input_files, output_file):
     '''Filter SNP Calls'''
     cmd_dict = CMD_DICT.copy()
@@ -184,9 +129,6 @@ def filter_snps(input_files, output_file):
     call(gatk_cmd, cmd_dict)
 
 stages_dict = {
-    'create_intervals': create_intervals,
-    'local_realignment': local_realignment,
-    'fix_realigned_mates': fix_mate_realigned,
     'indel_genotyping': indel_genotyping,
     'filter_indels': filter_indels,
     'snp_genotyping': snp_genotyping,
