ea7f98afac2ae521d0e32d0dc55717b4ed9189a6
Walter Scheper <scheper@unc.edu>
2010-09-09 14:46:16 -0400

    Updated paired end sam file generation
    * simplified naming of the stages to make command line calling of stages easier
    * added a tagging stage to add read group tags to reads
    * reworked paths to files: discared full paths in favor of paths relative to
      the working directory
    * updated pmsg calls to reflect new pmsg
    * removed MAX_RECORDS_IN_RAM=7500000 options from picard command lines

--- pipelines/convert_paired_end.py
+++ pipelines/convert_paired_end.py
@@ -17,46 +17,45 @@ from utils import call, paired_re, paired_strings, pmsg, read_group_re, saicmp, 
 
 # Copy sequence from staging area
 def copy_sequence_generator():
-    cwd = os.getcwd()
-    for file in glob('./staging_area/*'):
+    for file in glob('staging_area/*'):
         filename = '%(line)s_s_%(lane)s_%(pair)s_sequence.fastq.gz' % \
                 paired_re.search(file).groupdict()
-        yield [file, '%s/fastq/%s' % (cwd, filename)]
+        yield [file, 'fastq/%s' % (filename)]
 
 @follows(mkdir('fastq'))
 @files(copy_sequence_generator)
 @jobs_limit(2)
-def copy_sequences(input_file, output_file):
-    """Copy sequence files from staging area on thumper1"""
+def copy_sequence(input_file, output_file):
+    '''Copy sequence files from staging area'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
     cmd_dict['outfile_prefix'] = output_file.rstrip('.gz')
-    pmsg('Sequence Copy', input_file, cmd_dict['outfile_prefix'])
+    pmsg('Copying sequence files', input_file, cmd_dict['outfile_prefix'])
     SeqIO.convert(cmd_dict['infile'], 'fastq-illumina', cmd_dict['outfile_prefix'], 'fastq-sanger')
     pmsg('Compressing file', cmd_dict['outfile_prefix'], cmd_dict['outfile'])
     zip(cmd_dict['outfile_prefix'])
 
 @jobs_limit(2)
-@follows(mkdir('sai'))
-@transform(copy_sequences, regex(r'^(.*)/fastq/(.*)\.fastq\.gz$'), r'\1/sai/\2.sai')
+@follows(mkdir('sai'), mkdir('logs'))
+@transform(copy_sequence, regex(r'^fastq/(.+)\.fastq\.gz$'), r'sai/\1.sai')
 def fastq_to_sai(input_file, output_file):
     '''Convert FASTQ files to SAI files.'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
-    pmsg('FASTQ to SAI', cmd_dict['infile'], cmd_dict['outfile'])
     bwacmd = '%(bwa)s aln -t %(threads)s %(genome)s %(infile)s > %(outfile)s'
+    pmsg('Aligning sequences', cmd_dict['infile'], cmd_dict['outfile'])
     call(bwacmd, cmd_dict)
 
 # Merge paired ends to SAM
 @follows(mkdir('sam'))
-@transform(fastq_to_sai, regex(r'^(.*)/sai/(\w+)_s_(\d+)_1_sequence\.sai$'),
-           inputs([r'\1/sai/\2_s_\3_2_sequence.sai',
-                   r'\1/sai/\2_s_\3_1_sequence.sai',
-                   r'\1/fastq/\2_s_\3_1_sequence.fastq.gz',
-                   r'\1/fastq/\2_s_\3_2_sequence.fastq.gz']),
-            r'\1/sam/\2_s_\3.sam')
+@transform(fastq_to_sai, regex(r'^sai/(\w+)_s_(\d+)_1_sequence\.sai$'),
+           inputs([r'sai/\1_s_\2_2_sequence.sai',
+                   r'sai/\1_s_\2_1_sequence.sai',
+                   r'fastq/\1_s_\2_1_sequence.fastq.gz',
+                   r'fastq/\1_s_\2_2_sequence.fastq.gz']),
+            r'sam/\1_s_\2.sam')
 def paired_ends_to_sam(input_files, output_file):
     '''Convert SAI files and FASTQ files to SAM files.'''
 
@@ -71,7 +70,7 @@ def paired_ends_to_sam(input_files, output_file):
 
     cmd_dict = CMD_DICT.copy()
     assert type(input_files) is type([])
-    pmsg('SAM generation', ', '.join(input_files), output_file.strip('.gz'))
+    pmsg('Generating SAM file', ', '.join(input_files), output_file.strip('.gz'))
     # sort input files
     input_files.sort(cmp=saicmp)
     # Run bwa to merge paired ends into SAM file
@@ -82,57 +81,55 @@ def paired_ends_to_sam(input_files, output_file):
 
 ## Convert filtered SAM files to BAM files
 @follows(mkdir('bam'))
-@transform(paired_ends_to_sam, regex(r'^(.*)/sam/(.+)\.sam$'), r'\1/bam/\2.bam')
+@transform(paired_ends_to_sam, regex(r'^sam/(.+)\.sam$'), r'bam/\1.bam')
 def sam_to_bam(input_file, output_file):
     '''Convert SAM files to BAM files.'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
-    pmsg('SAM to BAM', cmd_dict['infile'], cmd_dict['outfile'])
+    pmsg('Converting SAM file to BAM', cmd_dict['infile'], cmd_dict['outfile'])
     sam_cmd = '%(samtools)s import %(genome)s.fai %(infile)s %(outfile)s'
     call(sam_cmd, cmd_dict)
 
 # Sort BAM file
 @follows(mkdir('sorted'))
-@transform(sam_to_bam, regex(r'^(.*)/bam/(.+)\.bam$'), r'\1/sorted/\2.sorted.bam')
+@transform(sam_to_bam, regex(r'^bam/(.+)\.bam$'), r'sorted/\1.sorted.bam')
 def sort_bam(input_file, output_file):
     '''Sort BAM files by coordinate.'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
     cmd_dict['outprefix'] = os.path.splitext(cmd_dict['outfile'])[0]
-    pmsg('BAM Coord Sort', cmd_dict['infile'], cmd_dict['outfile'])
+    pmsg('Cooridinate sorting BAM file', cmd_dict['infile'], cmd_dict['outfile'])
     picard_cmd = '%(picard)s SortSam ' + \
             'I=%(infile)s ' + \
             'O=%(outfile)s ' + \
             'SO=coordinate ' + \
-            'MAX_RECORDS_IN_RAM=7500000 ' + \
             'VALIDATION_STRINGENCY=SILENT'
     call(picard_cmd, cmd_dict)
 
 # Remove duplicates
 @follows(mkdir('deduped'))
-@transform(sort_bam, regex(r'^(.*)/sorted/(.*)\.sorted\.bam$'),
-        r'\1/deduped/\2.deduped.bam')
+@transform(sort_bam, regex(r'^sorted/(.+)\.sorted\.bam$'),
+        r'deduped/\1.deduped.bam')
 def remove_duplicates(input_file, output_file):
     '''Remove duplicates from BAM file'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
     cmd_dict['metrics'] = output_file.rstrip('bam') + 'metrics'
-    pmsg('Remove duplicates', input_file, output_file)
+    pmsg('Removing duplicates', input_file, output_file)
     picard_cmd = '%(picard)s MarkDuplicates ' + \
             'I=%(infile)s ' + \
             'O=%(outfile)s ' + \
             'M=%(metrics)s ' + \
-            'REMOVE_DUPLICATES=true' + \
-            'MAX_RECORDS_IN_RAM=7500000 ' + \
+            'REMOVE_DUPLICATES=true ' + \
             'VALIDATION_STRINGENCY=SILENT'
     call(picard_cmd, cmd_dict)
 
 # Update header with missing data
-@follows(mkdir('prepped'))
-@transform(remove_duplicates, regex(r'^(.*)/deduped/(.*)\.deduped\.bam$'), r'\1/prepped/\2.prepped.bam')
+@follows(mkdir('header'))
+@transform(remove_duplicates, regex(r'^deduped/(.+)\.deduped\.bam$'), r'header/\1.header.bam')
 def fix_header(input_file, output_file):
     '''Fix header info'''
     cmd_dict = CMD_DICT.copy()
@@ -143,38 +140,43 @@ def fix_header(input_file, output_file):
     open(cmd_dict['header_tmp'], 'w').write(
         open(cmd_dict['header_template'], 'r').read() % cmd_dict
     )
+    pmsg('Fixing header', input_file, output_file)
     picard_cmd = '%(picard)s ReplaceSamHeader ' + \
             'I=%(infile)s ' + \
             'O=%(outfile)s ' + \
             'HEADER=%(header_tmp)s ' + \
-            'MAX_RECORDS_IN_RAM=7500000 ' + \
             'VALIDATION_STRINGENCY=SILENT'
-
     call(picard_cmd, cmd_dict)
     os.remove(cmd_dict['header_tmp'])
 
-# Create index from BAM - creates BAI files
-@transform(fix_header, regex(r'^(.*)/prepped/(.+)\.bam'), r'\1/prepped/\2.bam.bai')
-def bam_index(input_file, output_file):
-    '''Index BAM file and create a BAI file.'''
-    pmsg('Create BAM Index', input_file, output_file)
+@follows(mkdir('prepped'))
+@transform(fix_header, regex('^header/(.+)\.header\.bam$'), r'prepped/\1.prepped.bam')
+def tag_reads(input_file, output_file):
+    '''Tag reads with read group'''
     cmd_dict = CMD_DICT.copy()
     cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
-    picard_cmd = '%(picard)s BuildBamIndex ' + \
-            'I=%(infile)s ' + \
+    cmd_dict['samfile'] = os.path.splitext(output_file)[0] + '.sam'
+    cmd_dict['read_group'] = read_group_re.match(input_file).groupdict()['read_group']
+    pmsg('Tagging reads', input_file, output_file)
+    command = '%(samtools)s view -h %(infile)s | ' + \
+            'sed "/^[^@].*XT:/s|$|\tRG:Z:%(read_group)s|" > %(samfile)s'
+    call(command, cmd_dict, is_logged=False)
+    pmsg('Compressing SAM file', cmd_dict['samfile'], output_file)
+    command = '%(picard)s SamFormatConverter ' + \
+            'I=%(samfile)s ' + \
             'O=%(outfile)s ' + \
-            'MAX_RECORDS_IN_RAM=7500000 ' + \
-            'OVERWRITE=true ' + \
+            'CREATE_INDEX=true ' + \
             'VALIDATION_STRINGENCY=SILENT'
-    call(picard_cmd, cmd_dict)
+    call(command, cmd_dict)
+    os.remove(cmd_dict['samfile'])
 
 @follows(mkdir('coverage'))
-@transform(bam_index, regex(r'^(.*)/prepped/(.+)\.prepped\.bam\.bai$'), r'\1/coverage/\2.coverage')
+@transform(tag_reads, regex(r'^prepped/(.+)\.prepped\.bam$'), r'coverage/\1.coverage')
 def calculate_coverage(input_file, output_file):
     '''Calculate coverage statistics'''
     cmd_dict = CMD_DICT.copy()
-    cmd_dict['infile'] = os.path.splitext(input_file)[0]
+    cmd_dict['infile'] = input_file
     cmd_dict['outfile'] = output_file
     pmsg('Coverage calculations', cmd_dict['infile'], cmd_dict['outfile'])
     gatk_cmd = '%(gatk)s -T DepthOfCoverage ' + \
@@ -188,14 +190,15 @@ def calculate_coverage(input_file, output_file):
     call(gatk_cmd, cmd_dict)
 
 stages_dict = {
-    'copy_sequences': copy_sequences,
-    'align_sequences': fastq_to_sai,
-    'make_sam': paired_ends_to_sam,
-    'make_bam': sam_to_bam,
-    'sort_bam': sort_bam,
+    'copy': copy_sequence,
+    'align': fastq_to_sai,
+    'sam': paired_ends_to_sam,
+    'bam': sam_to_bam,
+    'sort': sort_bam,
+    'dedupe': remove_duplicates,
     'fix_header': fix_header,
-    'index_bam': bam_index,
-    'calculate_coverage': calculate_coverage,
+    'tag': tag_reads,
+    'coverage': calculate_coverage,
     'default': calculate_coverage,
 }
 
